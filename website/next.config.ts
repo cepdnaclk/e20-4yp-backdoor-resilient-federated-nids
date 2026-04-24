87e5cbcfe4eca103e1bb2fc87bce0d58ef863eb6
Dineth Shakya <150885267+DinethShakya23@users.noreply.github.com>
2026-04-24 19:25:55 +0530

    remove nextjs webpage

--- website/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/e20-4yp-backdoor-resilient-federated-nids",
-  images: { unoptimized: true },
-};
-
-export default nextConfig;
